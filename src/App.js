a256884176274cf239fd99940a630fc182e0e2c6
ewanlaing <117400398+ewanlaing@users.noreply.github.com>
2023-06-27 08:58:30 +0100

    all up and running

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';    
+import './App.css';           
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+function App() {              
+  // we can write JS here :)  
+  const name = 'Ewan';
+  const age = '33';
+  return ( 
+    <div>                 
+       <h1>Hello {name}</h1>  
+       <h2>You are {age} years old.</h2>
     </div>
-  );
-}
+  );                          
+}                             
 
-export default App;
+export default App;           
